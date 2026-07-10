2e94851578bb16f5fe911e5f37ffcfbe3f35daa1
Melvin Alvarez <melvin_daf@hotmail.com>
2026-07-10 13:03:46 -0400

    Finishing Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string keepPlaying = "yes";
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int userCount = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                userCount++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+            }
+            if (userCount <= 1)
+            {
+                Console.WriteLine("Impressive! you guessed it right the first time!");
+            }
+            else
+            {
+                Console.WriteLine($"You have tried {userCount} times");            
+            }
+
+            Console.Write("Would you like to play again (yes/no)? ");
+            keepPlaying = Console.ReadLine().ToLower();
+            
+        } while (keepPlaying == "yes");
     }
 }
