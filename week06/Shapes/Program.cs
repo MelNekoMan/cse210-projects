1e1c0d24807b4002bde2fe8cd63fd0677f02fe3b
Melvin Alvarez <melvin_daf@hotmail.com>
2026-08-11 03:11:28 -0400

    Finishing Shapes Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Red", 10.5));
+        shapes.Add(new Circle("Green", 15));
+        shapes.Add(new Rectangle("Yellow", 12.9, 2.6));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
